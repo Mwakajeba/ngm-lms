ec045f3b37a7db87a92bf5b1f8fb5ba88da12e13
Anselim-ba <anselimbalthazar@gmail.com>
2025-07-29 11:28:26 +0300

    feat: Add models and migrations for GL Transactions, Receipts, Payments, Journals, and their items

---
+++ app/Models/JournalItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JournalItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'journal_id',
+        'chart_account_id',
+        'amount',
+        'nature',
+    ];
+
+    public function journal()
+    {
+        return $this->belongsTo(Journal::class);
+    }
+
+    public function chartAccount()
+    {
+        return $this->belongsTo(ChartAccount::class);
+    }
+}
