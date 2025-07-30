0d451015e325773b52f20873c6a4372ece06cde0
Julius Mwakajeba <jmwakajeba@gmail.com>
2025-07-30 17:58:17 +0300

    Refactor customer management: update routes, enhance views with breadcrumbs, and add loan officer relationships

--- routes/web.php
+++ routes/web.php
@@ -340,6 +340,16 @@ Route::middleware(['auth'])->group(function () {
 
 ////////////////////////////////////////////// END CUSTOMER MANAGEMENT ///////////////////////////////////////////
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('customers', [CustomerController::class, 'index'])->name('customers.index');
+    Route::get('customers/create', [CustomerController::class, 'create'])->name('customers.create');
+    Route::post('customers', [CustomerController::class, 'store'])->name('customers.store');
+    Route::get('customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
+    Route::get('customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+    Route::put('customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+    Route::delete('customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+});
+
 ////////////////////////////////////////////// CASHCOLLATERALS MANAGEMENT ///////////////////////////////////////////
 
 Route::middleware(['auth'])->prefix('cash_collaterals')->group(function () {
