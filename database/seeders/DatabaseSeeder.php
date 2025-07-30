2843f787bb23e607d3594a75ce57edca938e71d8
Renovat23 <revocatusba@gmail.com>
2025-07-30 09:51:13 +0300

    oya

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
       UserSeeder::class,
       RolePermissionSeeder::class,
       MenuSeeder::class,
-      AccountingClassGroupSeeder::class,
+      // AccountingClassGroupSeeder::class,
       AccountClassSeeder::class,
       CashFlowCategorySeeder::class,
       EquityCategorySeeder::class,
