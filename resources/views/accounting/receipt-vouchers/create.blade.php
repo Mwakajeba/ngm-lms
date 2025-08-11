7eeba7ddd4c9c290a2e970a9791571ed665aa91c
Julius Mwakajeba <jmwakajeba@gmail.com>
2025-08-11 18:27:08 +0300

    Enhance UI for payment and receipt vouchers with improved styling and button functionality

--- resources/views/accounting/receipt-vouchers/create.blade.php
+++ resources/views/accounting/receipt-vouchers/create.blade.php
@@ -14,7 +14,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card radius-10">
-                        <div class="card-header bg-primary text-white">
+                        <div class="card-header bg-secondary text-white">
                             <div class="d-flex align-items-center">
                                 <div>
                                     <h5 class="mb-0 text-white">
@@ -230,8 +230,8 @@
                                                     Total Amount: <span id="totalAmount">0.00</span>
                                                 </h4>
                                             </div>
-                                            <button type="submit" class="btn btn-success" id="saveBtn">
-                                                <i class="bx bx-save me-2"></i>Save
+                                            <button type="submit" class="btn btn-primary" id="saveBtn">
+                                                <i class="bx bx-plus-circle me-2"></i>Create Voucher
                                             </button>
                                         </div>
                                     </div>
