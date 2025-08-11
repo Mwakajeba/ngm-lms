5654cac137a03ee163f89fcdf8593a26f3e0c88c
mwandu-byte <mashakamwandu507@gmail.com>
2025-08-11 16:30:37 +0300

    Comment out unused authorization checks in loan products views to streamline code and improve maintainability.

--- resources/views/loan-products/show.blade.php
+++ resources/views/loan-products/show.blade.php
@@ -16,7 +16,7 @@
                     ['label' => 'Product Details', 'url' => '#', 'icon' => 'bx bx-info-circle']
                 ]" />
                 <div>
-                    @can('deactivate loan product')
+                    <!-- @can('deactivate loan product') -->
                     <button type="button" class="btn {{ $loanProduct->is_active ?? true ? 'btn-warning' : 'btn-success' }} toggle-status-btn"
                         title="{{ $loanProduct->is_active ?? true ? 'Deactivate' : 'Activate' }} Product"
                         data-product-id="{{ Hashids::encode($loanProduct->id) }}"
@@ -25,19 +25,19 @@
                         <i class="bx {{ $loanProduct->is_active ?? true ? 'bx-pause-circle' : 'bx-play-circle' }}"></i>
                         {{ $loanProduct->is_active ?? true ? 'Deactivate' : 'Activate' }}
                     </button>
-                    @endcan
+                    <!-- @endcan -->
 
-                    @can('edit loan product')
+                    <!-- @can('edit loan product') -->
                     <a href="{{ route('loan-products.edit', Hashids::encode($loanProduct->id)) }}" class="btn btn-primary">
                         <i class="bx bx-edit"></i> Edit Product
                     </a>
-                    @endcan
+                    <!-- @endcan -->
 
-                    @can('view loan product')
+                    <!-- @can('view loan product') -->
                     <a href="{{ route('loan-products.index') }}" class="btn btn-secondary">
                         <i class="bx bx-arrow-back"></i> Back to List
                     </a>
-                    @endcan
+                    <!-- @endcan -->
                 </div>
             </div>
 
