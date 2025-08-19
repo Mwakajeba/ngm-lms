a59b7bc76808b43951924b6b769cc0a8097006a8
Anselim-ba <anselimbalthazar@gmail.com>
2025-08-19 09:49:08 +0300

    fix: Update loan repayment export route name for clarity

--- routes/web.php
+++ routes/web.php
@@ -406,7 +406,7 @@ Route::prefix('accounting')->name('accounting.')->middleware('auth')->group(func
     ////////REPAYMENT ROUTE///////
     Route::get('/loan-repayments', [LoanReportController::class, 'getRepaymentReport'])->name('loans.reports.repayment');
     Route::get('/loan-repayment', [LoanReportController::class, 'getRepaymentReport'])->name('loans.reports.loan-repayment');
-    Route::get('/loan-repayment/export', [LoanReportController::class, 'exportLoanRepayment'])->name('loans.reports.loan-export');
+    Route::get('/loan-repayment/export', [LoanReportController::class, 'exportLoanRepayment'])->name('loans.reports.loan-export-repayment');
     // Loan Aging Report
     Route::get('/loan-aging', [LoanReportController::class, 'loanAgingReport'])->name('loans.reports.loan_aging');
 
