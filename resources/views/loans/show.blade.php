76193438722a59887aaf9b36086f193e04653005
efron damasco <efrondamasco@gmail.com>
2025-10-02 16:38:37 +0300

    debug: Simplify penalty removal condition in loan details view

--- resources/views/loans/show.blade.php
+++ resources/views/loans/show.blade.php
@@ -654,7 +654,7 @@
                                                                 <i class="bx bx-credit-card me-1"></i>Repay
                                                             </button>
                                                         @endif
-                                                        @if($item->penalty_amount > 0 && !$isPenaltyPaid)
+                                                        @if(!$isPenaltyPaid)
                                                             <button type="button" class="btn btn-sm btn-warning ms-1"
                                                                 onclick="removePenalty('{{ $item->id }}', '{{ number_format($item->penalty_amount, 2) }}')">
                                                                 <i class="bx bx-x-circle me-1"></i>Remove Penalty
