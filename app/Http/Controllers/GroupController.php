36a160208aed9b4c2733c683f1ab596f6d82e795
efron damasco <efrondamasco@gmail.com>
2025-10-02 14:30:15 +0300

    fix: Correct date format for joined_date in transferMember method

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -882,7 +882,7 @@ class GroupController extends Controller
 
             // Add member to target group
             $targetGroup->members()->attach($request->member_id, [
-                'joined_date' => now()->format('Y M D')
+                'joined_date' => now()->format('Y-m-d')
             ]);
             \Log::info('[GroupTransfer] Attached member to target group', [
                 'member_id' => $member->id,
