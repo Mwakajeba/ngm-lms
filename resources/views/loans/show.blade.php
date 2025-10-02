b1653594a8e8c03ddbc64f5efe66fe28015f99ad
efron damasco <efrondamasco@gmail.com>
2025-10-02 17:52:29 +0300

    fix: Correct penalty amount reference in LoanSchedule model and update view logic

--- resources/views/loans/show.blade.php
+++ resources/views/loans/show.blade.php
@@ -619,8 +619,9 @@
                                                     $isFullyPaid = $item->fullPrincipalPaid();
                                                     $paymentPercentage = $item->payment_percentage;
                                                     $completed = $loan->status === 'completed';
-                                                    $penaltyPaid = $item->repayments;
+                                                    $penaltyPaid = $item->PenaltyPaid();
                                                     $penaltAmount = $item->penalty_amount;
+                                                    dd($penaltyPaid, $penaltAmount);
 
                                                 @endphp
                                                 <tr
